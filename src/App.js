3911eed44530ac1818713e09031a17ed44c2c403
spencer-mcguire <smcguire1826@gmail.com>
2019-12-11 16:11:30 -0600

    buy and remove features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { buy, remove } from "./actions/featureActions";
 
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
@@ -8,22 +9,28 @@ import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
 const App = props => {
+  console.log(props);
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    props.remove(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.buy(item);
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures removeFeature={removeFeature} car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures
+          buyItem={buyItem}
+          additionalFeatures={props.additionalFeatures}
+        />
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -31,7 +38,7 @@ const App = props => {
 };
 
 const mapStateToProps = state => {
-  console.log(state);
+  // console.log(state);
   return {
     additionalPrice: state.additionalPrice,
     car: state.car,
@@ -39,7 +46,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, {})(App);
+export default connect(mapStateToProps, { buy, remove })(App);
 
 // const state = {
 //   additionalPrice: 0,
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,6 @@
+import { BUY_FEATURE, REMOVE_FEATURE } from "../actions/featureActions";
+import { bindActionCreators } from "../../../../../AppData/Local/Microsoft/TypeScript/3.6/node_modules/redux";
+
 const InitialState = {
   additionalPrice: 0,
   car: {
@@ -16,7 +19,27 @@ const InitialState = {
 };
 
 export const featureReducer = (state = InitialState, action) => {
+  console.log(state, action);
   switch (action.type) {
+    case BUY_FEATURE:
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
+        car: {
+          ...state.car,
+          features: [...state.car.features, action.payload]
+        }
+      };
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          features: state.car.features.filter(i => action.payload !== i.id),
+          additionalPrice: state.additionalPrice - action.payload.price
+        }
+      };
+
     default:
       return state;
   }
